c6f61101b0fb5fb4b53b4b3886ca6cd6bab4f417
kamilo116 <kamil.sladowski@gmail.com>
2018-12-13 19:36:32 +0100

    prune and check

--- main.pl
+++ main.pl
@@ -24,13 +24,19 @@ use Thread;
 sub print_help{
 print "This is the simulator, which generates containers in Docker, to eat memory disc size.
 At the same time, a second process works, which stops and removes containers that take up more than 1 MB disc space.
-There are available two parameters: -e, -c, which define how often each of the processes (allocation and cleaning) should be launched.
+There are available two parameters: --eat, --clean, which define how often each of the processes (allocation and cleaning) should be launched.
+Also you can use --prune to remove all unused containers, networks, images (both dangling and unreferenced), and optionally, volumes.
+The difference between --clean and --prune is that first kill demanding continers, the second deletes unused resources.
+
+
+The whole program works for about 100 second. 
+During this, you can monitor the status of disc usage, by running script with --check flag, on the second console,
+or manuall by executing following command:
+# docker system df -v
+
+
 Usage:
-
-The whole program works for about 100 second. During this, you can see the results by executing following command on the second console:
-docker system df -v
-
---help, --eat [1...5],  --clean [1...5]
+--help, --check, --eat [1...5],  --clean [1...5], --prune
 "
 }
 
@@ -38,7 +44,6 @@ sub run_spammer{
     my ($spamming_delay) = @_;
     for(1..5) {
         print "Spammer - start";
-        # my $container_id = run_spamming_container();
         run_spamming_container();
         print "Spammer - end";
         sleep $spamming_delay;
@@ -66,22 +71,20 @@ sub start_threads {
     my ($spamming_delay, $cleaning_delay) = @_;
     my $t1 = Thread->new(\&run_spammer, $spamming_delay);
     my $t2 = Thread->new(\&run_cleaner, $cleaning_delay);
-
-
-    # for(1..10){
     print "This is the main program\n";
-    # }
 
     print "Waiting for thread now\n";
     my $stuff1 = $t1->join();
     my $stuff2 = $t2->join();
     print "After join \n";
 
-    execute("cleanup_all_unused");
+    
 }
 my $eat_disc_factor = 1;
 my $clean_disc_frequency = 1;
-my $help     = 0;
+my $help = 0;
+my $check = 0;
+my $prune = 0;
 my $timeout = 100;
 
 
@@ -90,11 +93,18 @@ GetOptions(
     'eat=s'    => \$eat_disc_factor,
     'clean=i'     => \$clean_disc_frequency,
     'help!'     => \$help,
+    'check!'     => \$check,
+    'prune!'     => \$prune,
 ) or die "Incorrect usage!\n";
 
 if( $help ) {
     print_help();
-} else {
+    }
+elsif ($check){
+    print execute("system_usage_full");
+}
+else {
+  
     if(($eat_disc_factor ~~ [ 1, 2, 3, 4, 5]) && ($clean_disc_frequency ~~ [ 1, 2, 3, 4, 5])){
         print "correct patameters \n";
         $eat_disc_factor = 6 - $eat_disc_factor;
@@ -111,17 +121,11 @@ if( $help ) {
         print_help();
         die;
     }
+    
+    if($prune){
+        print "prune \n";
+        execute("cleanup_all_unused");
+    } 
 }
 
 
-
-
-# my $output = execute("system_usage");
-# print $output;
-#
-
-
-
-
-
-# start_cleaner_and_spammer($eat_disc_factor, $clean_disc_frequency);

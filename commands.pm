04ee3ef00363147ba434a26d83ce9c5602963246
kamilo116 <kamil.sladowski@gmail.com>
2018-12-13 19:07:54 +0100

    Update commands.pm

--- commands.pm
+++ commands.pm
@@ -1,49 +1,48 @@
 use 5.24.0;
 use strict;
 use warnings;
+use IPC::Run 'run';
 
 our ( @ISA, @EXPORT );
 require Exporter;
 @ISA    = qw(Exporter);
 @EXPORT = qw/execute execute_on/;
 
-# sub get_commands {
-    my %commands = ();
-    my @ps = split(/ /, "docker ps");
-    my @logs = split(/ /, "docker logs");
-    my @stats = split(/ /, "docker stats");
-    my @system_usage = split(/ /, "docker system df");
-    my @system_usage_full = split(/ /, "docker system df -v");
-    my @run_ubuntu = split(/ /, "docker run -itd ubuntu");
-    my @delete_dangling_volumes = ("docker", "volume", "rm", "$(docker volume ls -qf dangling=true)");
-    my @delete_dangling_images = ("docker", "rmi", "$(docker images -qf dangling=true)");
-    my @cleanup_all_unused = split(/ /, "docker system prune -a");  # [y]
+my %commands = ();
+my @ps = split(/ /, "docker ps");
+my @logs = split(/ /, "docker logs");
+my @stats = split(/ /, "docker stats");
+my @system_usage = split(/ /, "docker system df");
+my @system_usage_full = split(/ /, "docker system df -v");
+my @run_ubuntu = split(/ /, "docker run -itd ubuntu");
+my @delete_dangling_volumes = ("docker", "volume", "rm", "$(docker volume ls -qf dangling=true)");
+my @delete_dangling_images = ("docker", "rmi", "$(docker images -qf dangling=true)");
+my @cleanup_all_unused = split(/ /, "docker system prune -a");  # [y]
 
-    my @build_spammer_image = split(/ /, "docker build -t spammer .");
-    my @run_spamming_container= split(/ /, "docker run -itd spammer");
-    my @stop = split(/ /, "docker stop");
-    my @kill = split(/ /, "docker kill");
-    my @rm = split(/ /, "docker rm");
-    my @memory_usage = ("docker", "stats", "--all", "--format", "table {{.Container}}\t{{.CPUPerc}}\t{{.MemUsage}}", "--no-stream");
+my @build_spammer_image = split(/ /, "docker build -t spammer .");
+my @run_spamming_container= split(/ /, "docker run -itd spammer");
+my @stop = split(/ /, "docker stop");
+my @kill = split(/ /, "docker kill");
+my @rm = split(/ /, "docker rm");
+my @memory_usage = ("docker", "stats", "--all", "--format", "table {{.Container}}\t{{.CPUPerc}}\t{{.MemUsage}}", "--no-stream");
 
-    $commands{ps} = \@ps;
-    $commands{logs} = \@logs;
-    $commands{stats} = \@stats;
-    $commands{system_usage} = \@system_usage;
-    $commands{system_usage_full} = \@system_usage_full;
-    $commands{run_ubuntu} = \@run_ubuntu;
-    $commands{delete_dangling_volumes} = \@delete_dangling_volumes;
-    $commands{delete_dangling_images} = \@delete_dangling_images;
-    $commands{cleanup_all_unused} = \@cleanup_all_unused;
-    $commands{build_spammer_image} = \@build_spammer_image;
-    $commands{run_spamming_container} = \@run_spamming_container;
-    $commands{memory_usage} = \@memory_usage;
-    $commands{stop} = \@stop;
-    $commands{kill} = \@kill;
-    $commands{rm} = \@rm;
+$commands{ps} = \@ps;
+$commands{logs} = \@logs;
+$commands{stats} = \@stats;
+$commands{system_usage} = \@system_usage;
+$commands{system_usage_full} = \@system_usage_full;
+$commands{run_ubuntu} = \@run_ubuntu;
+$commands{delete_dangling_volumes} = \@delete_dangling_volumes;
+$commands{delete_dangling_images} = \@delete_dangling_images;
+$commands{cleanup_all_unused} = \@cleanup_all_unused;
+$commands{build_spammer_image} = \@build_spammer_image;
+$commands{run_spamming_container} = \@run_spamming_container;
+$commands{memory_usage} = \@memory_usage;
+$commands{stop} = \@stop;
+$commands{kill} = \@kill;
+$commands{rm} = \@rm;
 
-#     return %commands;
-# }
+
 
 
 sub execute {

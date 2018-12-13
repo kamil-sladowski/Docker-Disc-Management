09d3e2f9bc3f14d732238953c48998369dc5529e
kamilo116 <kamil.sladowski@gmail.com>
2018-12-13 19:57:29 +0100

    logs

--- container.pm
+++ container.pm
@@ -13,35 +13,29 @@ sub build_spammer_img{
 sub run_spamming_container{
     my $container_id = execute("run_spamming_container");
     $container_id = substr( $container_id, 0, 12 );
-    print $container_id . "\n";
+    print "INFO: Started container " . $container_id . "\n";
     return $container_id;
 }
 
 sub get_running_containers{
     my @containers_ids = ();
     my $output = execute("ps");
-    print $output;
     my @lines = split "\n", $output;
     foreach my $line (@lines){
         if (index($line, "CONTAINER ID") == -1) {
-            print "LINE " . $line . "\n";
+            print "INFO: Running container " . $line . "\n";
             my @container_record = split / /, $line;
-print @container_record;
+            print @container_record;
             push(@containers_ids, $container_record[0]);
-            print "added $container_record[0] \n";
         }
     }
-    # if(scalar @match != 0){
     return @containers_ids;
-    # }
 }
 
 
 sub is_container_eat_to_much{
     my ($container_id) = @_;
-    print "container_id  " . $container_id ;
     my $output = execute("memory_usage");
-    print $output;
     my @lines = split "\n", $output;
     foreach my $line (@lines){
         if (index($line, $container_id) != -1) {
@@ -50,6 +44,7 @@ sub is_container_eat_to_much{
             my $size =  $disc_usage[2];
             my $unit = $disc_usage[3];
             if ("MiB" eq $unit || "GiB" eq $unit ){
+                print "INFO: Container " . $container_id . " eat to much resources\n";
                 return 1;
             }
             last;
